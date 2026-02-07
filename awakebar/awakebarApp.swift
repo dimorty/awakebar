c0bfb9363c093680bdd217ef413a45c93cdcb822
Dmitry Verkhov <dimorty21@gmail.com>
2026-02-07 11:56:13 +0300

    Initial Commit

---
+++ awakebar/awakebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  awakebarApp.swift
+//  awakebar
+//
+//  Created by Dmitry Verkhov on 07.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct awakebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
